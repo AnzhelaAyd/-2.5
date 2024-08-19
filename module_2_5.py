728a394f317b9be050d501d6521405fd46d9c61e
AnzhelaAyd <angiemagomedova@yandex.com>
2024-08-19 23:02:06 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,18 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix.append(value)
+    return matrix
+
+
+n = int(input('n= '))
+m = int(input('m= '))
+value = int(input('value= '))
+
+result = get_matrix(n, m, value)
+if n == 0 or m == 0:
+    print('Данная матрица не существует ')
+else:
+    print(result)
